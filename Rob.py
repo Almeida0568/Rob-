299cf03c2435a94caa754b728bf3c48d3ef6d8bb
Almeida0568 <87243791+Almeida0568@users.noreply.github.com>
2021-07-14 16:17:41 +0100

    aqui vai

---
+++ Rob.py
@@ -0,0 +1,37 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import speech_recognition as sr
+from gtts import gTTS
+from playsound import playsound
+
+
+def ouvir_som():
+    microfone = sr.Recognizer()
+    with sr.Microphone() as source:
+        microfone.adjust_for_ambient_noise(source)
+        print("Microfone...")
+        audio = microfone.listen(source)
+        try:
+            frase = microfone.recognize_google(audio, language='pt-BR')
+        except sr.UnknownValueError:
+            print("Bot: Isso não funcionou...")
+
+def cria_audio(audio):
+    tts = gTTS(audio, language='pt-BR')
+    tts.save("Bot.mp3")
+    playsound("Bot.mp3")
+
+
+bot = ChatBot('Chat')
+
+conversas = ["oi",'olá!','Tudo bem?','Tudo e contigo','Eu tou bem','Que bom','Qual a melhor linguagem?','a melhor é python']
+
+trainer = ListTrainer(bot)
+trainer.train(conversas)
+
+while True:
+    quest = input("Humano: ")
+    resposta = bot.get_response(quest)
+    print("Bot: ",resposta)
+    
+
